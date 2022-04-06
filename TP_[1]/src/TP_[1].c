0457ecbbbc7febcd29fcd48b536da7c73b3b0bb8
LeanIsaac <leancarp09@gmail.com>
2022-04-06 20:33:20 -0300

    primer push

---
+++ TP_[1]/src/TP_[1].c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TP_[1].c
+ Author      : MARECOS LEANDRO
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("TP 1"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
